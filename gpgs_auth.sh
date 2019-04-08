d1a0b435c302608d96e763538a155b961ce07167
Alexey Gulev <Alexey.Gulev@king.com>
2019-04-08 13:09:18 +0200

    New deps for gpgs auth

--- gpgs_auth.sh
+++ gpgs_auth.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python pompom.py --pom https://maven.google.com/com/google/android/gms/play-services-games/17.0.0/play-services-games-17.0.0.pom --pom https://dl.google.com/dl/android/maven2/com/google/android/gms/play-services-auth/16.0.0/play-services-auth-16.0.0.pom --out gpgs poms
+python pompom.py --pom https://dl.google.com/dl/android/maven2/com/google/android/gms/play-services-auth/16.0.0/play-services-auth-16.0.0.pom --pom https://maven.google.com/com/google/android/gms/play-services-ads/17.2.0/play-services-ads-17.2.0.pom --out gpgs poms
 python pompom.py --out gpgs -apv 28 deps
