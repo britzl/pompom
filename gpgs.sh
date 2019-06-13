73283fc6dc5a7511981b4c5da3f639f0112b7c76
Alexey Gulev <Alexey.Gulev@king.com>
2019-06-13 16:50:33 +0200

    Login process

--- gpgs.sh
+++ gpgs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python pompom.py --pom https://maven.google.com/com/google/android/gms/play-services-games/17.0.0/play-services-games-17.0.0.pom --out gpgs poms
+python pompom.py --pom https://maven.google.com/com/google/android/gms/play-services-games/15.0.0/play-services-games-15.0.0.pom --out gpgs poms
 python pompom.py --out gpgs -apv 28 deps
