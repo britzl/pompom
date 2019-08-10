28b3ac5f1091ecbf0a4a8ea25205d165d17829a7
Alexey Gulev <Alexey.Gulev@king.com>
2019-08-10 15:02:51 +0200

    firebase analytics fix

--- firebase_analytics.sh
+++ firebase_analytics.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python pompom.py --pom https://maven.google.com/com/google/firebase/firebase-analytics/16.0.5/firebase-analytics-16.4.0.pom --out firebase_analytics poms
+python pompom.py --pom https://maven.google.com/com/google/firebase/firebase-analytics/16.0.5/firebase-analytics-16.0.5.pom --out firebase_analytics poms
 python pompom.py --out firebase_analytics --exclude exceptions/defold.json --exclude exceptions/firebase_core.json -apv 28 deps
